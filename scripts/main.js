dc11e1873a98f8186766cb7c62cfedd6731a1c9c
thomas-at-mages-edu-sg <thomas@mages.edu.sg>
2026-02-11 13:41:49 +0800

    Initial files by Terry

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+import { gsap } from "gsap";
+import { _numWithUnitExp } from "gsap/gsap-core";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// Register the ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".square1", {
+  x: 1000,
+  duration: 8,
+  scrollTrigger: {
+    trigger: ".square1",
+    start: "top 80%",
+    end: "top 30%",
+    scrub: true,
+    // scrub: 4,
+    toggleActions: "restart none none none", // onEnter onLeave onEnterBack onLeaveBack
+    markers: {
+      startColor: "white",
+      endColor: "red",
+      fontSize: "20px",
+      indent: 100,
+    }
+  }
+});
